ace91ccc4b245253f2cd0dfbed1256d7383b8333
swswgumenyuk <swgumenyuk@gmail.com>
2022-04-11 14:48:42 +0300

    створення проекту 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+          <h1>Tom</h1>
+          <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT5sPDe4AR18d03OnMGkC-CEje-1KeKwJrIhIdXFjVpa6MW3jner6YapG6Pp2woc3Q024&usqp=CAU'} alt={'Tom'}/>
+      </div>
+        <div>
+          <h1>Jerry</h1>
+          <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm2OprYAMm5HxTbTzaX5Ks5kuiHWd2Vcnmphv2ozfIcC4FIBNbYZeh1YtqPW67p6Fk424&usqp=CAU'} alt={'Jerry'}/>
+
+      </div>
     </div>
   );
 }
